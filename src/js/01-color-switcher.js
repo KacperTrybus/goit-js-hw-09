99a38d3bee3bd967659079e8aed67dcfcff5fc64
KacperTrybus <117526502+KacperTrybus@users.noreply.github.com>
2023-09-14 23:12:26 +0200

    1st task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+let interval;
+
+function changeBgColor() {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+}
+
+function startInterval() {
+  interval = setInterval(changeBgColor, 1000);
+  startBtn.setAttribute('disabled', '');
+}
+
+function stopInterval() {
+  clearInterval(interval);
+  startBtn.removeAttribute('disabled');
+}
+
+startBtn.addEventListener('click', startInterval);
+stopBtn.addEventListener('click', stopInterval);
