37a8bb7d1d934f21c61e9970fc1b0136835fa9a3
KacperTrybus <117526502+KacperTrybus@users.noreply.github.com>
2023-09-16 00:41:03 +0200

    task 3 not done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,29 @@
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    console.log('fullfilled');
+    // Fulfill aaaa
   } else {
+    console.log('rejected');
     // Reject
   }
 }
+
+let maxTime;
+const firstDelay = document.querySelector('input[name=delay]');
+const stepDelay = document.querySelector('input[name=step]');
+const amountInput = document.querySelector('input[name=amount]');
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function (event) {
+  event.preventDefault();
+  const firstDelayValue = Number(firstDelay.value);
+  const stepDelayValue = Number(stepDelay.value);
+  const amount = Number(amountInput.value);
+
+  setTimeout(createPromise, firstDelayValue);
+  clearTimeout(createPromise);
+  for (let i = 2; i <= amount; i++) {
+    setTimeout(createPromise, stepDelayValue);
+  }
+});
