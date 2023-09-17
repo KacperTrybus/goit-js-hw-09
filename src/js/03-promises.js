420f529da603d18d2a410e95f9bfb76f6e4fcf0f
KacperTrybus <117526502+KacperTrybus@users.noreply.github.com>
2023-09-18 01:20:27 +0200

    3 done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,29 +1,51 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    console.log('fullfilled');
-    // Fulfill aaaa
-  } else {
-    console.log('rejected');
-    // Reject
-  }
-}
-
-let maxTime;
 const firstDelay = document.querySelector('input[name=delay]');
 const stepDelay = document.querySelector('input[name=step]');
 const amountInput = document.querySelector('input[name=amount]');
 const btn = document.querySelector('button');
+const form = document.querySelector('.form');
 
-btn.addEventListener('click', function (event) {
+function createPromise(position, delay) {
+  const shouldResolve = Math.random() > 0.3;
+  const promise = new Promise((resolve, reject) => {
+    //nowy promise dla ktorego robi sie funkcja
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  promise // wywoluje promise
+    .then(({ position, delay }) => {
+      //jak resolved
+      console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      // jak rejected
+      console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
+form.addEventListener('submit', function (event) {
   event.preventDefault();
+  btn.setAttribute('disabled', '');
+
   const firstDelayValue = Number(firstDelay.value);
   const stepDelayValue = Number(stepDelay.value);
   const amount = Number(amountInput.value);
 
-  setTimeout(createPromise, firstDelayValue);
-  clearTimeout(createPromise);
-  for (let i = 2; i <= amount; i++) {
-    setTimeout(createPromise, stepDelayValue);
+  let createdPromiseCount = 0;
+
+  for (let i = 1; i <= amount; i++) {
+    const currentDelay = firstDelay + (i - 1) * step; // zapisywanie delaya dla loga
+    createPromise(i, currentDelay).then(() => {
+      createdPromiseCount++; // zwiekszenie licznika wykonanych funkcji
+
+      if (createdPromiseCount === amount) {
+        submitButton.removeAttribute('disabled');
+      }
+    });
   }
 });
